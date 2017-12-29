ad2ff7f60c6711dd65c6dc6da287d879d147cc2e
jpintoriv <jorgepintoriveros@gmail.com>
2017-12-29 14:06:36 -0300

    working

--- Sender.py
+++ Sender.py
@@ -13,79 +13,76 @@ from PIL import Image
 import random
 import string
 import os
+from app import *
 
 class Sender:
 
-    def __init__(self):
-        pass
+    def __init__(self, message, name_google_archive):
+        self.pref = ''.join(random.sample(string.hexdigits, 8))
+        self.message = message
+        self.name_google_archive = name_google_archive
+        self.src_img_qr = None
+        self.driver = None
 
-
-    def generate_pref(self):
-        return ''.join(random.sample(string.hexdigits, 8))
-
-    def send_messages(self, message, name_google_archive, app_web):
-        pref = self.generate_pref()
-        #crea prefijo
-
+    def connect(self):
         # Crear el perfil de firefox
         profile = webdriver.FirefoxProfile()
         # Dar las opciones de firefox para permitir el cambio de ventana con contenido en el dom
         profile.set_preference("dom.disable_beforeunload",True)
         profile.set_preference("javascript.enabled", False)
         # Crear el driver con las opciones que deseamos
-        driver = webdriver.Firefox(profile)
+        self.driver = webdriver.Firefox(profile)
         # levantar el firefox controlado con la pagina web whatsapp
-        driver.get("https://web.whatsapp.com/")
+        self.driver.get("https://web.whatsapp.com/")
 
+        return '''<meta http-equiv="refresh" content="0;URL='/good_connection'" />'''
 
-        img_qr = driver.find_element_by_xpath("//img[@alt='Scan me!']")
-        src_img_qr = img_qr.get_attribute("src")
-        driver.get_screenshot_as_file(pref+'_screenshot.png')
+    def get_qr(self):
+        img_qr = self.driver.find_element_by_xpath("//img[@alt='Scan me!']")
+        self.src_img_qr = img_qr.get_attribute("src")
+        self.driver.get_screenshot_as_file(self.pref+'_screenshot.png')
         pos = img_qr.location
         siz = img_qr.size
         x_i = pos['x'] - 15
         y_i = pos['y'] - 15
         x_f = int(siz['width'] + 15 + pos['x'])
         y_f = int(siz['height'] + 15 + pos['y'])
-        img = Image.open(pref+'_screenshot.png')
+        img = Image.open(self.pref+'_screenshot.png')
         img2 = img.crop((x_i,y_i,x_f,y_f))
-        img2.save(pref+'_crop.png')
-        # conseguir el QR
-        app_web.wait_qr(pref)
+        img2.save(self.pref+'_crop.png')
 
-        time.sleep(10)
-        # todo lo que pasa axa es para que el usuario pueda escanear el codigo QR se puede aumentar si se prefiere pero esto yo lo encontre optimo
+        return '''<meta http-equiv="refresh" content="0;URL='/wait_qr?pref='''+self.pref+'''" />'''
 
+    def get_new_qr(self):
         change_page = False
 
         while not change_page:
             try:
-                new_img = driver.find_element_by_xpath("//img[@alt='Scan me!']")
-                if new_img.get_attribute("src") != src_img_qr:
+                new_img = self.driver.find_element_by_xpath("//img[@alt='Scan me!']")
+                if new_img.get_attribute("src") != self.src_img_qr:
                     print("change image QR")
-                    img_qr = driver.find_element_by_xpath("//img[@alt='Scan me!']")
-                    src_img_qr = img_qr.get_attribute("src")
-                    driver.get_screenshot_as_file(pref+'_screenshot.png')
+                    img_qr = self.driver.find_element_by_xpath("//img[@alt='Scan me!']")
+                    self.src_img_qr = img_qr.get_attribute("src")
+                    self.driver.get_screenshot_as_file(self.pref+'_screenshot.png')
                     pos = img_qr.location
                     siz = img_qr.size
                     x_i = pos['x'] - 15
                     y_i = pos['y'] - 15
                     x_f = int(siz['width'] + 15 + pos['x'])
                     y_f = int(siz['height'] + 15 + pos['y'])
-                    img = Image.open(pref+'_screenshot.png')
+                    img = Image.open(self.pref+'_screenshot.png')
                     img2 = img.crop((x_i,y_i,x_f,y_f))
-                    img2.save(pref+'_crop.png')
-                    app_web.wait_qr(pref)
+                    img2.save(self.pref+'_crop.png')
+                    return '''<meta http-equiv="refresh" content="0;URL='/new_qr?pref='''+self.pref+'''" />'''
             except:
-                app_web.working()
-                change_page = True            
-            time.sleep(1)
-        #espera hasta que se cambia la pagina, en caso de que cambia el QR tambien lo cambia
-        
+                change_page = True
+        return '''<meta http-equiv="refresh" content="0;URL='/working'" />'''
+            
+    def send_messages(self):
         print("entry page")
 
-        os.remove(pref+'_screenshot.png')
-        os.remove(pref+'_crop.png')
+        os.remove(self.pref+'_screenshot.png')
+        os.remove(self.pref+'_crop.png')
         print("remove images")
         #borra los archivos creados
 
@@ -95,7 +92,7 @@ class Sender:
         client = gspread.authorize(creds)
         # se "obtiene" el excel en este script se le pasa el nombre por argumentos en esta version pero cambiando el metodo puede por el link ACORDARSE COMPARTIR EL DOCUMENTO 
         # con el usuario que se creo en el client secret o con el usuario de que son las credenciales
-        sheet = client.open(name_google_archive).sheet1
+        sheet = client.open(self.name_google_archive).sheet1
         # tomar contenido del shet y guardalo en una variable
         list_of_hashes = sheet.get_all_records()
         pat = re.compile(r'\s+')
@@ -103,21 +100,23 @@ class Sender:
         for name in list_of_hashes:
             # se crea el mensaje si se quiere crear un mensaje tipo se agrega a esta cadena
         
-            string = message.replace("(nombre)",(name['Nombre']))
+            string = self.message.replace("(nombre)",(name['Nombre']))
             #se reemplaza (nombre) por nombre del contacto
 
-            driver.get("https://web.whatsapp.com/send?phone="+pat.sub('',str(name['Telefono']))+"")
+            self.driver.get("https://web.whatsapp.com/send?phone="+pat.sub('',str(name['Telefono']))+"")
             # se guarda el cuadro de texto por la class 
             inp_xpath = '//div[@class="pluggable-input-body copyable-text selectable-text"][@dir="auto"][@data-tab="1"]'
-            wait = WebDriverWait(driver, 600)
+            wait = WebDriverWait(self.driver, 600)
             input_box = wait.until(EC.presence_of_element_located((By.XPATH, inp_xpath)))   
             # se manda la tecla enter y un mensaje ya creado
-            time.sleep(1)         
+            time.sleep(3)         
             input_box.send_keys(string + Keys.ENTER)
-            time.sleep(4)
+            time.sleep(2)
         # termina el ciclo y se termina la ejecucion del firefox zombie
-        app_web.success()
-        driver.close()
+        self.driver.close()
+        return '''<meta http-equiv="refresh" content="0;URL='/success'" />'''
+
+
 
 
 
--- app.py
+++ app.py
@@ -2,12 +2,33 @@ import cherrypy
 from Sender import *
 import threading
 import sys
+import os
+import time
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-class Massive_Wsp():
+class Massive_Wsp(object):
+
 	def __init__(self):
-		pass
+		self.sender = None
+
+	def redirect_connecting(self):
+		raise cherrypy.HTTPRedirect("./connecting")
+
+	def redirect_qr(self, pref):
+		raise cherrypy.HTTPRedirect("./wait_qr?pref="+pref)
+		#return "<head><meta http-equiv='refresh' content='0; url=./wait_qr?pref="+pref+"' /></head>"
+
+	def redirect_new_qr(self, pref):
+		raise cherrypy.HTTPRedirect("./new_qr?pref="+pref)
+
+	def redirect_working(self):
+		raise cherrypy.HTTPRedirect("./working")
+
+	def redirect_success(self):
+		raise cherrypy.HTTPRedirect("./success")
 	
 	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
 	def index(self):
 		return """<html>
           <head></head>
@@ -21,34 +42,59 @@ class Massive_Wsp():
             </form>
           </body>
         	</html>"""
+		
 
 
 
 	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
 	def send_message(self,message, google_archive):
-		t = threading.Thread(target=Sender().send_messages,args=(message,google_archive,self,))
-		t.start()
-		return "ingresando a web.whatsapp.com"
+		self.sender = Sender(message,google_archive)
+		#return self.sender.send_messages(message,google_archive,self)
+		yield "conectando con whatsap web"
+		yield self.sender.connect()
 
 	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
+	def good_connection(self):
+		yield "Conexion realizada con exito"
+		yield self.sender.get_qr()
+
+
+	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
 	def wait_qr(self, pref):
-		print("wait_qr")
 		img = pref+"_crop.png"
-		return """Escanee la imagen QR:
-    		<br><br>
-    		<img src="""+img+""">"""
+		yield """Escanee la imagen QR:
+    		<br><br>"""+'<img src="images/'+img+'" border = "0"/>'
+		time.sleep(1)
+		yield self.sender.get_new_qr()
 
 	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
+	def new_qr(self, pref):
+		img = pref+"_crop.png"
+		yield """La imagen QR cambió:
+    		<br><br>"""+'<img src="images/'+img+'" border = "0"/>'
+		yield self.sender.get_new_qr()
+
+	@cherrypy.expose
+	@cherrypy.config(**{'response.stream': True})
 	def working(self):
-		return "QR aceptada"
+		yield "QR aceptada"
+		yield self.sender.send_messages()
 
 	@cherrypy.expose
 	def success(self):
 		return "envio exitoso"
 
-    
+     
 
 	
 
 if __name__ == '__main__':
-    cherrypy.quickstart(Massive_Wsp())
+	conf = {   "/images":
+		 			{"tools.staticdir.on": True,
+	                "tools.staticdir.dir": dir_path},
+	}
+	cherrypy.quickstart(Massive_Wsp(),'/',config=conf)
