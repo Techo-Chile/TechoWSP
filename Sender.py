e12ca08f0ddbe5f7633723e2042fe98ccae1de31
jpintoriv <jorgepintoriveros@gmail.com>
2018-01-04 15:33:32 -0300

    last edits

--- Sender.py
+++ Sender.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import *
 import re
 import gspread
